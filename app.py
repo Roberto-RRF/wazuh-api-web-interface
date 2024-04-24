374730588babf99057a6d49519ee87f86d87cbf1
Luis <0234598@up.edu.mx>
2024-04-23 19:21:40 -0600

    add functions as a package, create bases for top_10 and common

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import sys
+sys.path.append('src')
+from . import _api_calls
 
 app = Flask(__name__)
 
@@ -6,6 +9,26 @@ app = Flask(__name__)
 def home():
     return render_template(
         "index.html"
+    )
+
+@app.route("/top_10")
+def top_10():
+    countries = [
+        ('United States', '65%'),
+        ('UK', '15.7%'),
+        ('Russia', '5.8%'),
+        ('Spain', '2.1%'),
+        ('India', '1.9%'),
+        ('France', '1.5%')
+    ]
+    header=_api_calls.get_header()
+    print(header)
+    return render_template('top_10.html', countries=countries)
+
+@app.route("/common")
+def common():
+    return render_template(
+        "common.html"
     )
 
 if __name__ == "__main__":
---
+++ functions/_api_calls.py
@@ -0,0 +1,42 @@
+import json
+import requests
+import urllib3
+from base64 import b64encode
+from operator import itemgetter 
+
+# Desactivar las advertencias de https inseguro (para certificados SSL autofirmados)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Configuración de la conexión a la API
+protocol = 'https'
+host = '54.149.137.67'
+# host = "172.25.180.72"
+port = 55000
+user = 'wazuh'
+password = 'ePdVeStfbVuujM2eGG5*WQ.1vNLvz675'
+# user = "wazuh"
+# password = "wazuh"
+login_endpoint = 'security/user/authenticate'
+
+# Proceso de registro con la información básica
+login_url = f"{protocol}://{host}:{port}/{login_endpoint}"
+basic_auth = f"{user}:{password}".encode()
+login_headers = {'Content-Type': 'application/json',
+                 'Authorization': f'Basic {b64encode(basic_auth).decode()}'}
+
+# Solicitud del token para hacer uso de la API
+response = requests.post(login_url, headers=login_headers, verify=False)
+if response.status_code == 200:
+    token = json.loads(response.content.decode())['data']['token']
+    requests_headers = {'Content-Type': 'application/json',
+                        'Authorization': f'Bearer {token}'}
+else:
+    requests_headers = None
+    exit()
+
+
+def get_header():
+    return requests_headers
+
+def get_host() -> str:
+    return host
